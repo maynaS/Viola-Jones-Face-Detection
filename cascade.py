51e80467dff776a3d05ad390e7ec30037cf45e7f
keyur <keyur@pop-os.localdomain>
2022-12-02 20:53:55 +0530

    add boosted, cascade and face detection

---
+++ cascade.py
@@ -0,0 +1,47 @@
+import pickle
+
+
+class CascadeClassifier:
+    def __init__(self, layers):
+        self.layers = layers
+        self.clfs = []
+
+    def train(self, training):
+        positive = []
+        negative = []
+
+        for i in range(len(training)):
+            if training[i][1] == 1:
+                positive.append(training[i])
+            else:
+                negative.append(training[i])
+        
+        for i in range(len(self.layers)):
+            if len(negative) == 0:
+                print("Stopping early")
+                break
+            
+            classifier = ViolaJones(T = i)
+            classifier.train(positive, negative)
+            self.clfs.append(classifier)
+            false_positives = []
+            for i in range(len(negative)):
+                if self.classify(negative[i][0]) == 1:
+                    false_positives.append(negative[i])
+
+            negative = false_positives
+    
+    def classify(self, img):
+        for classifier in self.clfs:
+            if classifier.classify(img) == 0:
+                return 0
+        return 1
+
+    def save(self, file_name):
+        with open(file_name + ".pkl", 'wb') as f:
+            pickle.dump(self, f)
+
+    @staticmethod
+    def load(file_name):
+        with open(file_name + ".pkl", 'rb') as f:
+            return pickle.load(f)
